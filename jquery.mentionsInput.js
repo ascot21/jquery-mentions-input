78c2ce2982471fa7b9dbdf5e8c262287859feb72
Adam Scott <ascot21@gmail.com>
2016-01-11 10:45:07 -0800

    Only trigger then dropdown when there is a space before the @
    ie. typing `test@example.com` should NOT trigger the dropdown

--- jquery.mentionsInput.js
+++ jquery.mentionsInput.js
@@ -324,10 +324,15 @@
             updateMentionsCollection();
 
             var triggerCharIndex = _.lastIndexOf(inputBuffer, settings.triggerChar); //Returns the last match of the triggerChar in the inputBuffer
+            var previousChar = inputBuffer[triggerCharIndex - 1]; //Returns the last match of the key pressed before triggerChar
             if (triggerCharIndex > -1) { //If the triggerChar is present in the inputBuffer array
-                currentDataQuery = inputBuffer.slice(triggerCharIndex + 1).join(''); //Gets the currentDataQuery
-                currentDataQuery = utils.rtrim(currentDataQuery); //Deletes the whitespaces
-                _.defer(_.bind(doSearch, this, currentDataQuery)); //Invoking the function doSearch ( Bind the function to this)
+                if (!previousChar || !previousChar.trim()) { //If there is no previous previousChar or the previousChar is a space
+                    currentDataQuery = inputBuffer.slice(triggerCharIndex + 1).join(''); //Gets the currentDataQuery
+                    currentDataQuery = utils.rtrim(currentDataQuery); //Deletes the whitespaces
+                    _.defer(_.bind(doSearch, this, currentDataQuery)); //Invoking the function doSearch ( Bind the function to this)
+                } else {
+                    hideAutoComplete();
+                }
             } else {
                 hideAutoComplete();
             }
